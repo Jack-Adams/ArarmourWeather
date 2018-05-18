e3f66f4828ee2ca1e8b50d58d68ffbb640eff566
Jack Adams <jack.addamz@gmail.com>
2018-05-19 05:57:32 +1200

    Adding .gitignore and creating file for map class

--- mapStructures.py
+++ mapStructures.py
@@ -1,1 +1,12 @@
-#
+# Author: Jack Adams
+# Date Started: 18/05/17
+# Last Updated: 18/05/17
+
+# This file contains the definitions of the map and point structures. It also
+# contains all of the methods which act on those structures.
+
+class map():
+    """
+
+
+    """
